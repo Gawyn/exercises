3d846d1e0abf89e4132e10b374bfed3caca7d937
Gawyn <cplanas.work@gmail.com>
2012-01-18 11:32:38 +0200

    Acknowledgements to Paolo

--- ex1/ex1.rb
+++ ex1/ex1.rb
@@ -1,1 +1,2 @@
 #Make that the result of every arithmetic add operation (1+1) is one unit bigger than expected (1+1=3)
+#Exercise taken from Metaprogramming Ruby, by Paolo Perrotta (great book, btw).
